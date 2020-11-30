d79fd048480cb1ef637d781b213a4b08f7302756
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-11-30 16:30:19 -0700

    random node generator method

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-const Stack = require("./classes/stack");
-const Node = require("./classes/node");
 const Maze = require("./classes/maze");
 
-const stack = new Stack();
-const node1 = new Node(0, 0);
-const node2 = new Node(1, 0);
-
-stack.push(node1).push(node2);
-console.log(stack);
-
 const maze = new Maze(5, 5);
-console.log(maze);
-// stack.push("hello");
-// console.log(stack);
-// console.log(stack.length);
+// console.log(maze);
+// console.log(maze.findNode(3, 2));
 
-// const peekItem = stack.peek();
-// console.log(peekItem);
-
-// const poppedItem = stack.pop();
-// console.log(poppedItem);
+console.log(maze.randomNode());
--- classes/maze.js
+++ classes/maze.js
@@ -1,8 +1,30 @@
+const Node = require("./node");
+
 module.exports = class Maze {
   constructor(width, length) {
     this.width = width;
     this.length = length;
-    // this.visited = false;
     this.nodes = {};
+    this.initializeNodes();
+  }
+
+  // this is the grid
+  initializeNodes() {
+    for (let y = 0; y < this.length; y++) {
+      for (let x = 0; x < this.width; x++) {
+        this.nodes[`${x}, ${y}`] = new Node(x, y);
+      }
+    }
+  }
+
+  findNode(x, y) {
+    return this.nodes[`${x}, ${y}`];
+  }
+
+  // seed of the node -- where creation begins
+  randomNode() {
+    const randomX = Math.floor(Math.random() * this.width);
+    const randomY = Math.floor(Math.random() * this.length);
+    return this.findNode(randomX, randomY);
   }
 };
